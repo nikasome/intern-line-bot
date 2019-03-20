ce88992e2385ebdfb42c26a9a86a09860194d7a7
gifteeintern-D <gifteeintern-d@giftee-no-MacBook-Pro.local>
2019-03-20 14:53:35 +0900

    add JiroInfo return

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,9 +1,13 @@
 require 'line/bot'
+require 'net/http'
+require 'uri'
+require 'json'
 
 class WebhookController < ApplicationController
   protect_from_forgery except: [:callback] # CSRF対策無効化
   
 	HAVE_A_JIRO = ["品川", "船橋", "池袋", "八王子", "新宿", "調布", "目黒"]
+	KEY_ID = "7cea63677b2284213dd45b3b2fbf8a99"
 
   def client
     @client ||= Line::Bot::Client.new { |config|
@@ -26,18 +30,15 @@ class WebhookController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-					result = ""
+					message = {		#default message
+            	type: 'text',
+            	text: '二郎ありません'
+         	}
 					if HAVE_A_JIRO.include?(event.message['text'])
-						result =  "二郎あります"
-					else
-						result =  "二郎ありません"
+						result = getInfo(event.message['text'])
+						message['text'] = result[0] + "\n" + result[1] + "\n" + result[2]
 					end
-					#when /日本/, /関東/, /東京.?/ then
-					#	return "範囲が広すぎます"
-          message = {
-            type: 'text',
-            text: result
-          }
+
           client.reply_message(event['replyToken'], message)
         when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
           response = client.get_message_content(event.message['id'])
@@ -48,4 +49,20 @@ class WebhookController < ApplicationController
     }
     head :ok
   end
+
+	def getInfo(area)
+		url = URI.parse("https://api.gnavi.co.jp")
+		http = Net::HTTP.new(url.host, url.port)
+		http.use_ssl = true
+		params = URI.encode_www_form({keyid: KEY_ID, name: 'ラーメン二郎', freeword: area}) 
+		req = Net::HTTP::Get.new("/RestSearchAPI/v3/?#{params}")
+		res = http.request(req)
+		api_response = JSON.parse(res.body)
+
+		return api_result = [
+			api_response['rest'][0]['name'],		#店舗名
+			api_response['rest'][0]['address'],	#店舗住所
+			api_response['rest'][0]['url']			#店舗URL
+		]
+	end
 end
